f3d993df73680b65f5389ebb77b13350506f5fd8
egor <egor555.novik@gmail.com>
2023-10-06 15:11:52 +0300

    change UserCreateDto.cs

--- UserApp.Common/DTOs/UserCreateDto.cs
+++ UserApp.Common/DTOs/UserCreateDto.cs
@@ -1,14 +1,25 @@
-﻿namespace UserApp.Common.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserApp.Common.DTOs;
 
 public class UserCreateDto
 {
+    [Required]
+    [DataType(DataType.Text)]
     public string? Name { get; set; }
     
+    [Required]
+    [Range(0, 150)]
     public int Age { get; set; }
     
+    [Required]
+    [DataType(DataType.EmailAddress)]
     public string? Email { get; set; }
     
+    [Required]
+    [DataType(DataType.Password)]
     public string? Password { get; set; }
     
+    [Required]
     public List<int>? Roles { get; set; }
 }
